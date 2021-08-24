72ec7df9c8fb415d7abf05407209efa91e895f83
dcsuh <daniel.c.suh@gmail.com>
2021-08-24 09:10:16 -0400

    read in data and start exploring

---
+++ scripts/becker-han-exploration.R
@@ -0,0 +1,11 @@
+## Daniel Suh
+## August 24, 2021
+## Playing around with data from Becker & Han 2020 GBE
+
+library(tidyverse)
+library(magrittr)
+library(here)
+
+data <- read_csv(here("data/Becker Han 2020 GEB_gpf & BRT data.csv"))
+
+dat1 <- data %>% select(X1,genus,common,inames,bcomp) %>% distinct()
