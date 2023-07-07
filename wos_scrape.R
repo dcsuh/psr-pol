8b448b3866c17b6dd841838a7af4174f7f279f00
dcsuh <daniel.c.suh@gmail.com>
2023-07-07 11:55:37 -0400

    trying to figure out this web scraping

--- wos_scrape.R
+++ wos_scrape.R
@@ -1,52 +1,65 @@
-## Scrape web of science citations
-## source code from Tad Dallas: https://gist.github.com/taddallas/cbf0589df1fa9051d774b2d282b6e635
+## RSelenium method to scrape citation counts from WOS
+
+## Currently set up to work with the RSelenium docker image
+## https://cran.r-project.org/web/packages/RSelenium/vignettes/RSelenium-docker.html 
 
 
-# RSelenium method to scrape citation counts from WOS
 
-**Updates**: RSelenium package has switched some of the code around, causing breaking changes to this old gist. Web of Science has also changed their layout, causing breaking changes. The current code is a hacky patch to allow for some WoS interactivity and data acquistion. 
-
-
-### Currently set up to work with the RSelenium docker image
 
 library(RSelenium)
 library(plyr); library(dplyr)
 
-driver <- rsDriver(port = 4545L, 
-                   browser = "firefox", 
-                   version = "latest"
-)
+checkForServer()
+startServer()
 
- # https://resulumit.com/teaching/scrp_workshop.html#190
+remDr <- remoteDriver()
 
-browser <- driver$client
-server <- driver$server
 
-browser$navigate("https://www.webofscience.com/wos/woscc/advanced-search")
+remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4445L, browserName = "chrome'")
+
+remDr$open()
+
+## replace the link below with a fresh advanced search link
+remDr$navigate("https://apps.webofknowledge.com/WOS_AdvancedSearch_input.do?SID=1EwJfvSCrzymRmVr9i5&product=WOS&search_mode=AdvancedSearch")
+
+
+# Loop to get citation data for each page of results, each iteration will save a txt file,
+# I used selectorgadget to check the css ids, they might be different for you.
+#Get the host names, loop through the web of science page using them
+# Note: query below is a vector of host names to search through Web of Science.
+# The default is to search NAMES + "parasite"
 
 query <- c('mus musculus', 'gorilla gorilla')
 
+
 ret <- vector()
+
 
 for(i in 1:length(query)){
   
   # find where to enter text, and then enter text
-  webElem <- browser$findElement(using = 'css', value = "textarea.search-criteria-input")
-  webElem$sendKeysToElement(list(paste("TS=('", query[i], "' AND 'parasite')", sep=' '), "\uE007")) 
+  webElem <- remDr$findElement(using = 'css', value = "div textarea")
+  webElem$sendKeysToElement(list(paste("TS=(", query[i], "AND 'parasite')", sep=' ')))
   
-  Sys.sleep(5)
-  browser$goBack()
-  Sys.sleep(5)
+  # wait a few seconds
+  Sys.sleep(10)
   
-  webElem <- browser$findElement(using = 'css', value = "textarea.search-criteria-input")
-  webElem$clearElement()
+  # click on 'Search'
+  webElem2 <- remDr$findElement(using = "css", "span input")
+  webElem2$clickElement()
+  
+  # wait a few seconds
+  Sys.sleep(10)
+  
+  # attempt to snag the citation count, if it doesn't exist, clear the element
+  err <- tryCatch(unlist(remDr$findElement(using='css', '.newErrorHead')), error = function(e){})
+  if(!is.null(err)){remDr$findElement(using = 'css', value = "div textarea")$clearElement()}
+  
+  # extract the citation counts from the html page and put count into vector element (ret)
+  tab <- html(unlist(remDr$getCurrentUrl()))
+  ret[i] <- trimws(tab %>% html_node(paste("#set_",i,"_div",sep='')) %>% html_text())
   
   print(i)
+  print(ret[i])
   
 }
-
-webElem <- browser$findElement(using = 'css', value = "app-history-entries-list.ng-star-inserted")
-ret <- webElem$getElementText() 
-
-driver[['server']]$stop()
-
